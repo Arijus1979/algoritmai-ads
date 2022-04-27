29f9313dd509430e0308d7b2c0532170dbc23459
Arijus1979 <arijusta@gmail.com>
2022-04-27 15:20:37 +0300

    Pabaigtas

--- ads/ads.cpp
+++ ads/ads.cpp
@@ -1,20 +1,59 @@
-// ads.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Pirminiu dvyniu paieskos algoritmas
 
-#include <iostream>
+#include "bits/stdc++.h"
+
+using namespace std;
+
+
+void printTwinPairs(int n)
+{
+    int P[1000];
+    P[0] = 2;
+    P[1] = 3;
+    int m, flag = 0, it=2;
+
+    cout << "(3, 5)" << endl;
+    for (int k = 1; k < n; k++)
+    {
+        m = (6 * k - 1)/2;
+        for (int i = 2; i <= m; i++)
+        {
+            if ((6 * k - 1) % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            P[it] = 6 * k - 1;
+            it++;
+            m = (6 * k + 1)/2;
+            for (int j = 2; j <= m; j++)
+            {
+                if ((6 * k + 1) % j == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                P[it] = 6 * k + 1;
+                it++;
+                cout << "(" << 6 * k - 1 << ", " << 6 * k + 1 << ")" << endl;
+            }
+        }
+        flag = 0;
+    }
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int n = 1000; // iki kokio k norite ieskoti pirminiu skaiciu poru?
+
+    printTwinPairs(n);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
